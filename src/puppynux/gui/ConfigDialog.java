85810fd61b6b329919f35e1d4c6ac3abee3a9e66
William20 <william.rozenberg@gmail.com>
2016-04-05 11:44:27 +0200

    Update all

--- src/puppynux/gui/ConfigDialog.java
+++ src/puppynux/gui/ConfigDialog.java
@@ -2,12 +2,10 @@ package puppynux.gui;
 
 import puppynux.gui.data.Choices;
 import puppynux.gui.data.ConfigDialogInfo;
-import puppynux.gui.data.FirstDialogInfo;
-import puppynux.gui.listeners.CancelListener;
-import puppynux.gui.listeners.OKListener;
-import puppynux.rg.GameEngine;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,21 +20,21 @@ public class ConfigDialog extends JDialog implements PuppyDialog {
     private MainWindow mainWindow;
     private boolean sendData;
     private JPanel controlPanel, contentPanel;
-    private JLabel nameLabel, envLabel;
+    private JLabel nameLabel, envLabel, learnSpeedLabel, refreshLabel, velocityLabel;
     private JTextField name;
-    private JComboBox <String> environment;
-
+    private JComboBox<String> environment;
+    private JSlider learnSpeedSlider, refreshSlider;
+    private JSpinner velocitySpinner;//TODO
     /**
-     *
      * @param parent Parent frame
-     * @param title Dialog's title
+     * @param title  Dialog's title
      * @param modal
      */
     public ConfigDialog(JFrame parent, String title, boolean modal) {
         super(parent, title, modal);
 //        setSize(550, 270);
         mainWindow = (MainWindow) parent;
-        setLocationRelativeTo(null);
+//        setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
         initComponent();
@@ -45,6 +43,7 @@ public class ConfigDialog extends JDialog implements PuppyDialog {
 
     /**
      * Allows to show ConfigDialog
+     *
      * @return Info managed by ConfigDialogInfo
      */
     public ConfigDialogInfo showDialog() {
@@ -60,23 +59,64 @@ public class ConfigDialog extends JDialog implements PuppyDialog {
         //Name
         JPanel agentPanel = new JPanel();
         agentPanel.setBorder(BorderFactory.createTitledBorder("Agent's attributes"));
-        nameLabel = new JLabel("Select name :");
+        agentPanel.setLayout(new GridLayout(0,1,0,10));
+        nameLabel = new JLabel("Select name :", JLabel.CENTER);
         name = new JTextField();
         name.setColumns(12);
-        agentPanel.add(nameLabel);
-        agentPanel.add(name);
+        JPanel agentContentPanel = new JPanel();
+        agentContentPanel.add(nameLabel);
+        agentContentPanel.add(name, JPanel.CENTER_ALIGNMENT);
+        agentPanel.add(agentContentPanel);
+
+        learnSpeedLabel = new JLabel("Learn spead : ", JLabel.CENTER);
+        learnSpeedSlider = new JSlider();
+        learnSpeedSlider.setMaximum(10);
+        learnSpeedSlider.setMinimum(0);
+        learnSpeedSlider.setValue(10);
+        JLabel learnValueLabel = new JLabel(String.valueOf((double) learnSpeedSlider.getValue() / 10));
+        learnSpeedSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                learnValueLabel.setText(String.valueOf((double) learnSpeedSlider.getValue() / 10));
+            }
+        });
+        JPanel sliderPanel = new JPanel();
+        sliderPanel.add(learnSpeedLabel);
+        sliderPanel.add(learnSpeedSlider);
+        sliderPanel.add(learnValueLabel);
+        agentPanel.add(sliderPanel);
+
+        refreshLabel = new JLabel("Refresh frequency : ", JLabel.CENTER);
+        refreshSlider = new JSlider();
+        refreshSlider.setMaximum(10);
+        refreshSlider.setMinimum(0);
+        refreshSlider.setValue(10);
+        JLabel refreshValueLabel = new JLabel(String.valueOf((double) refreshSlider.getValue() / 10));
+        refreshSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                refreshValueLabel.setText(String.valueOf((double) refreshSlider.getValue() / 10));
+            }
+        });
+        JPanel refreshSliderPanel = new JPanel();
+        refreshSliderPanel.add(refreshLabel);
+        refreshSliderPanel.add(refreshSlider);
+        refreshSliderPanel.add(refreshValueLabel);
+        agentPanel.add(refreshSliderPanel);
+
 
         JPanel envPanel = new JPanel();
         envPanel.setBorder(BorderFactory.createTitledBorder("Environment's attribute"));
         envLabel = new JLabel("Choose environement : ");
-        String [] elements = {"Env1", "Env2"};
-        environment = new JComboBox<> (elements);
+        String[] elements = {"Env1", "Env2"};
+        environment = new JComboBox<>(elements);
         JButton edit = new JButton("Edit");
         envPanel.add(envLabel);
         envPanel.add(environment);
         envPanel.add(edit);
 
         contentPanel = new JPanel();
+        contentPanel.setLayout(new GridLayout(0, 2));
         contentPanel.add(agentPanel);
         contentPanel.add(envPanel);
 
@@ -118,6 +158,7 @@ public class ConfigDialog extends JDialog implements PuppyDialog {
 
     /**
      * Allows to make ConfigDialog visible
+     *
      * @param b
      */
     @Override
@@ -126,7 +167,6 @@ public class ConfigDialog extends JDialog implements PuppyDialog {
     }
 
     /**
-     *
      * @param sendData
      */
     @Override
