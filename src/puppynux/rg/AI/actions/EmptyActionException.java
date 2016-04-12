031353f4a6b4e0af5e3fbbf9c39d4f8cace3553c
William20 <william.rozenberg@gmail.com>
2016-04-12 15:50:37 +0200

    Update

---
+++ src/puppynux/rg/AI/actions/EmptyActionException.java
@@ -0,0 +1,14 @@
+package puppynux.rg.AI.actions;
+
+import java.util.EmptyStackException;
+
+/**
+ * Created by niamor972 on 07/04/16.
+ * Parts of puppynux.rg.AI.actions.
+ * >
+ */
+public class EmptyActionException extends Exception {
+    public EmptyActionException (String message) {
+        super(message);
+    }
+}
