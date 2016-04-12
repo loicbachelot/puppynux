0326270c667566406dd86c54dcd194b34b82563b
William20 <william.rozenberg@gmail.com>
2016-04-12 13:17:41 +0200

    Update before merge

---
+++ src/puppynux/gui/objects/Table.java
@@ -0,0 +1,11 @@
+package puppynux.gui.objects;
+
+/**
+ * Created by william on 12/04/16.
+ */
+public class Table extends Objects {
+    public Table(String image, int x, int y) {
+        super("src/resources/img/table.png", x, y);
+    }
+}
+
