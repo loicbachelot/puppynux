45fd505ad1410864c6a2dbd9ef43190f37f90d58
William20 <william.rozenberg@gmail.com>
2016-04-21 23:22:23 +0200

    Update first panel "AnimationPanel"

--- src/puppynux/gui/components/AnimationPanel.java
+++ src/puppynux/gui/components/AnimationPanel.java
@@ -1,5 +1,7 @@
 package puppynux.gui.components;
 
+import puppynux.gui.MainWindow;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,10 +13,14 @@ import java.awt.*;
 public class AnimationPanel extends JPanel{
 
     JLabel loadingLabel;
+    PuppynuxLabel mainTitleLabel;
 
     public AnimationPanel() {
-        loadingLabel = new JLabel("", new ImageIcon(getClass().getClassLoader().getResource("resources/img/loading.gif")), JLabel.CENTER);
+        mainTitleLabel = new PuppynuxLabel("Welcome to PUPPYNUX", JLabel.CENTER, "mainTitle");
+        loadingLabel = new JLabel("", new ImageIcon(getClass().getClassLoader().getResource("resources/img/animation.gif")), JLabel.CENTER);
+        this.setLayout(new GridLayout(0,1));
+        this.add(mainTitleLabel);
         this.add(loadingLabel);
-        setBackground(new Color(174,215,247));
+        setBackground(MainWindow.backgroundsColor);
     }
 }
